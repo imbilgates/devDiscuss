c58be9e233726934a9c165c74c94a0016196d8f8
bilgates27 <bilgates270703@gmail.com>
2025-03-21 18:03:58 +0530

    Add tooltips to buttons in Button component for improved accessibility and user guidance

--- client/src/component/Button.jsx
+++ client/src/component/Button.jsx
@@ -3,6 +3,8 @@ import add from '../assets/add.png';
 import explore from '../assets/explore.png';
 import profile from '../assets/profile.png';
 import compiler from '../assets/compiler.png';
+import Tooltip from '@mui/material/Tooltip';
+
 
 const Button = () => {
   const navigate = useNavigate();
@@ -22,23 +24,31 @@ const Button = () => {
   const handleCompilerClick = () => {
     window.open('/compiler', '_blank');
   };
-  
+
 
 
   return (
     <div className="d-flex justify-content-center align-items-center mt-4">
-      <button onClick={handleListClick} className="btn p-0 m-2">
-        <img src={explore} alt="List Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
-      </button>
-      <button onClick={handleAddClick} className="btn p-0 m-2">
-        <img src={add} alt="Add Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
-      </button>
-      <button onClick={handleProfileClick} className="btn p-0 m-2">
-        <img src={profile} alt="Add Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
-      </button>
-      <button onClick={handleCompilerClick} className="btn p-0 m-2">
-        <img src={compiler} alt="Add Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
-      </button>
+      <Tooltip title="Explore">
+        <button onClick={handleListClick} className="btn p-0 m-2">
+          <img src={explore} alt="List Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
+        </button>
+      </Tooltip>
+      <Tooltip title="Add">
+        <button onClick={handleAddClick} className="btn p-0 m-2">
+          <img src={add} alt="Add Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
+        </button>
+      </Tooltip>
+      <Tooltip title="Profile">
+        <button onClick={handleProfileClick} className="btn p-0 m-2">
+          <img src={profile} alt="Profile Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
+        </button>
+      </Tooltip>
+      <Tooltip title="Compiler">
+        <button onClick={handleCompilerClick} className="btn p-0 m-2">
+          <img src={compiler} alt="Compiler Discussion" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
+        </button>
+      </Tooltip>
     </div>
   );
 };
